86be5e29d6148d35821cb5f80048682700cd89ae
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-04-27 14:29:48 -0500

    Marcado de directorio de los paquetes

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# contenido de src/__init__.py
+from .tablero import Tablero
+from .jugador import Jugador
+from .juego import Juego
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# contenido de tests/__init__.py
+from .test_tablero import TestTablero
+from .test_jugador import TestJugador
+from .test_juego import TestJuego
