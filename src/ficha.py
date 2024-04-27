41f097f86d36ce74e2180fa8d16da2fbce0f9a52
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-04-27 15:28:57 -0500

    Clase Ficha (dama) terminada

--- src/ficha.py
+++ src/ficha.py
@@ -1,7 +1,16 @@
-# from src import Tablero, Jugador, Juego, Dado
+class Ficha: # Representa a una dama
+    def __init__(self, color, estado): # Inicializa una ficha con un color y un estado
+        self.color = color # rojo (dr) o amarillo (da)
+        self.estado = estado  # 'ordinaria (o)', 'finalista' (f), 'capturada (c)', 'libre (l)'
 
-class Ficha:
-    def __init__(self, color, estado):
-        # Inicializa una ficha con un color y un estado
-        self.color = color
-        self.estado = estado  # 'finalista', 'capturada', 'libre'
+    def convertir_en_finalista(self): # Convierte una ficha en finalista
+        self.estado = 'finalista'
+    
+    def convertir_en_ordinaria(self): # Convierte una ficha en ordinaria
+        self.estado = 'ordinaria'
+
+    def convertir_en_capturada(self): # Convierte una ficha en capturada
+        self.estado = 'capturada'
+    
+    def convertir_en_libre(self): # Convierte una ficha en libre
+        self.estado = 'libre'
