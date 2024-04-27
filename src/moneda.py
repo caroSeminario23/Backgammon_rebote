588dbb7ef5e6b808cbad89c0b4e3c5939abba180
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-04-27 15:19:21 -0500

    Clase Moneda terminada

--- src/moneda.py
+++ src/moneda.py
@@ -1,6 +1,12 @@
 import random
 
 class Moneda:
-    def __init__(self):
-        # Inicializa la moneda a 'a' (avanza), 'r' (retrocede) o 'i' (inmóvil) al azar
-        self.estado_actual = random.choice(['a', 'r', 'i'])
+    def __init__(self): # Inicializa la moneda con 'i' (inmóvil)
+        self.estado_actual = 'i'
+    
+    def lanzar_moneda(self): # Lanza la moneda y devuelve un estado aleatorio a: avanzar, r: retroceder
+        self.estado_actual = random.choice(['a', 'r']) # Actualiza el estado de la moneda con un valor aleatorio
+        return self.estado_actual # Devuelve el estado de la moneda
+    
+    def esperar_lanzamiento(self): # Espera a que la moneda se detenga
+        self.estado_actual = 'i' # Actualiza el estado de la moneda a inmóvil
