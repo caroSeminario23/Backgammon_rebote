bcde767a145c74468c6122032fa1fc362cd7b588
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-04-27 15:34:45 -0500

    Clase turno completado

--- src/turno.py
+++ src/turno.py
@@ -1,6 +1,11 @@
 import random
 
-class Turno:
-    def __init__(self):
-        # Inicializa el turno a 'R' (rojo) o 'A' (amarillo) al azar
-        self.turno_actual = random.choice(['R', 'A'])
+class Turno: 
+    def __init__(self, turno): # El primer jugador elije el turno que desea
+        self.turno_actual = turno # Inicializa el turno a 'R' (rojo) o 'A' (amarillo) al azar
+
+    def cambio_de_turno(self): # Cambia el turno al siguiente jugador
+        if self.turno_actual == 'R':
+            self.turno_actual = 'A'
+        else:
+            self.turno_actual = 'R'
