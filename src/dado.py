24c8e941c693ea83118fed95bf33feaed3de6fc9
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-04-27 15:11:34 -0500

    Clase dado terminada en funcionamiento

--- src/dado.py
+++ src/dado.py
@@ -1,11 +1,7 @@
-# from src import Tablero, Jugador, Juego, Ficha, Turno, Moneda
 import random
+class Dado: 
+    def __init__(self): # Inicializa un dado ordinario con valores del 1 al 6
+        self.valores = list(range(1, 7)) # [1, 2, 3, 4, 5, 6]
 
-class Dado:
-    def __init__(self):
-        # Inicializa un dado ordinario
-        self.valores = list(range(1, 7))
-
-    def lanzar(self):
-        # Devuelve un valor aleatorio entre 1 y 6
-        return random.choice(self.valores)
+    def lanzar(self): # Lanza el dado y devuelve un valor aleatorio
+        return random.choice(self.valores) # Devuelve un valor aleatorio de la lista de valores
