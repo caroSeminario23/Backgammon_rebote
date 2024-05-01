92ad47c5256b28b7ddf73afe775b5298a4baa586
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-05-01 16:27:19 -0500

    Dibujo de recuadros y adición de fuente

--- interfaz/tablero.py
+++ interfaz/tablero.py
@@ -18,17 +18,44 @@ ROJO = hex_to_rgb("#e47f83")
 PLOMO = hex_to_rgb("#FCFCFC")
 
 # Definir dimensiones de la ventana
-ANCHO = 1300
-ALTO = 700
+# Obtener las dimensiones de la pantalla
+pantalla = pygame.display.Info()
+ANCHO = pantalla.current_w
+ALTO = pantalla.current_h
 
 # Crear la ventana
-ventana = pygame.display.set_mode((ANCHO, ALTO)) # Crear la ventana con las dimensiones definidas
+ventana = pygame.display.set_mode((ANCHO, ALTO))
 
 # Función para dibujar el tablero
 def dibujar_tablero():
     ventana.fill(PLOMO)
+    # Agregar un titulo a la ventana
+    pygame.display.set_caption("Backgammon rebote")
+    # Dibujar 1 recuadro de color melon oscuro (izquierda)
+    pygame.draw.rect(ventana, MELON_OSCURO, (ANCHO//33, ALTO//6.8, ANCHO//4, ALTO/1.33)) # El primer argumento indica la ventana, el segundo el color, el tercero la posición y el cuarto el tamaño
+    # Dibujar 1 recuadro de color melon oscuro (superior derecha)
+    pygame.draw.rect(ventana, MELON_OSCURO, (ANCHO//3.35, ALTO//6.8, ANCHO//1.48, ALTO//11.7))
+    # Dibujar 1 recuadro de color melon oscuro (inferior derecha)
+    pygame.draw.rect(ventana, MELON_OSCURO, (ANCHO//3.35, ALTO//4, ANCHO//1.48, ALTO/1.54))
+    # Importación de la fuente para el titulo
+    fTitulo = pygame.font.Font('fuentes/Inter-ExtraBold.ttf', ALTO//15)
+    # Agregar un texto en la ventana
+    titulo = fTitulo.render("BACKGAMMON REBOTE", True, MELON_OSCURO)
+    ventana.blit(titulo, (ANCHO//3.8, ALTO//21))
+
+    # Dibujar 1 recuadro de color melon claro (izquierda superior)
+    pygame.draw.rect(ventana, MELON_CLARO, (ANCHO//22, ALTO//5.5, ANCHO//4.6, ALTO//4.8))    
+    # Dibujar 1 recuadro de color melon claro (izquierda medio)
+    pygame.draw.rect(ventana, MELON_CLARO, (ANCHO//22, ALTO//2.4, ANCHO//4.6, ALTO//4.8))
+    # Dibujar 1 recuadro de color melon claro (izquierda inferior)
+    pygame.draw.rect(ventana, MELON_CLARO, (ANCHO//22, ALTO//1.53, ANCHO//4.6, ALTO//4.8))
+
+    ''''
     for i in range(13):
-        pygame.draw.line(ventana, ROJO, (i * ANCHO // 12, 0), (i * ANCHO // 12, ALTO), 1)
+        color = ROJO if i % 2 == 0 else ROJO
+        pygame.draw.rect(ventana, color, (i * ANCHO // 12, ALTO // 3, ANCHO // 12, ALTO // 3))
+        pygame.draw.line(ventana, AMARILLO, (i * ANCHO // 12, 0), (i * ANCHO // 12, ALTO), 1)
+    '''
     pygame.display.update()
 
 # Bucle principal del juego
