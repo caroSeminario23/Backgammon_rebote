3f8be8ee765d2280ad954b6f7879249530e05f53
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-05-04 21:00:18 -0500

    Creacion de clase para colores y modificaciones

--- interfaz/registro.py
+++ interfaz/registro.py
@@ -1,25 +1,15 @@
 import pygame
 import pygame_gui
 import sys
+from utilidades.colores import dibujar_rectangulo_redondeado, VERDE, AMARILLO, NEGRO
 
 # Inicializar Pygame
 pygame.init()
 
-# Función para convertir un color en formato hexadecimal a RGB
-def hex_to_rgb(hex):
-    hex = hex.lstrip('#')
-    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
-
-# Definir colores
-NEGRO = hex_to_rgb("#000000")
-VERDE = hex_to_rgb("#a8c99e")
-PLOMO = hex_to_rgb("#D9D9D9")
-AMARILLO = hex_to_rgb("#efd8a4")
-
 # Crear una ventana
 ventana = pygame.display.set_mode((800, 400))
 
-# Crear un administrador de interfaz de usuario 
+# Crear un administrador de interfaz de usuario
 manager = pygame_gui.UIManager((800, 400))
 
 # Dibujar en la pantalla
@@ -29,16 +19,6 @@ def dibujar_pantalla_Registro():
     # Agregar un titulo a la ventana
     pygame.display.set_caption("Registro de jugadores")
 
-    # Función para dibujar un rectángulo con esquinas redondeadas
-    """Dibuja un rectángulo con esquinas redondeadas en Pygame."""
-    def dibujar_rectangulo_redondeado(surface, color, rect, radius):
-        x, y, width, height = rect
-        pygame.draw.circle(surface, color, (x+radius, y+radius), radius)
-        pygame.draw.circle(surface, color, (x+width-radius, y+radius), radius)
-        pygame.draw.circle(surface, color, (x+radius, y+height-radius), radius)
-        pygame.draw.circle(surface, color, (x+width-radius, y+height-radius), radius)
-        pygame.draw.rect(surface, color, (x, y+radius, width, height-2*radius))
-        pygame.draw.rect(surface, color, (x+radius, y, width-2*radius, height))
 
     # Dibujar un rectángulo con esquinas redondeadas de color verde
     dibujar_rectangulo_redondeado(ventana, AMARILLO, (27, 29, 746, 343), 11) 
@@ -77,7 +57,7 @@ def dibujar_pantalla_Registro():
 texto_pseudonimo1 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((216, 161), (170, 34)), manager=manager) # El primer parametro es la posición y el segundo el tamaño
 # Suponiendo que 'texto_pseudonimo1' es tu objeto UITextEntryLine
 
-# Crear casilla de texto para pseudonimo 1
+# Crear casilla de texto para pseudonimo 2
 texto_pseudonimo2 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((572, 161), (170, 34)), manager=manager) 
 # Suponiendo que 'texto_pseudonimo1' es tu objeto UITextEntryLine
 
@@ -147,15 +127,11 @@ while is_running:
 
     dibujar_pantalla_Registro()
 
-        
-
     manager.update(time_delta)
 
     #ventana.fill((255, 255, 255))
 
     manager.draw_ui(ventana)
-
-    
 
     pygame.display.update()
 
