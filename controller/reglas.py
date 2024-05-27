8b456f764b1c4cec2e1ba63b5db7e29a9df22ee3
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-05-27 09:21:47 -0500

    regla 9

--- controller/reglas.py
+++ controller/reglas.py
@@ -1278,7 +1278,141 @@ def actualizar_estado_CDAO(estado, xi, yi, xf, yf):
     estado.mostrar_estado()
     return estado
 
+# ==================
+#   REGLA 9: LDRC
+# ==================
+
+# Verificador de la posicion a la que se movera la ficha
+def verificador_LDRC(ficha, estado, posiciones):
+    aO = ficha.obtenerPosicion()[0]
+    bO = ficha.obtenerPosicion()[1]
+    a, b = 0, 0
+
+    # Transformar la coordenada de pantalla en valor del arreglo tablero
+    coord = (aO, bO)  
+    pos = encontrar_posicion_cercana(coord, posiciones)
+    if pos is not None:
+        print(f'La coordenada {coord} está cerca de la posición {pos} en self.posiciones_fichas')
+        a = pos[0]
+        b = pos[1]
+    else:
+        print(f'La coordenada {coord} no está cerca de ninguna posición en self.posiciones_fichas')
+
+    n = (estado.get_dado().get_valor_actual())-1
+    print(n, a, b)
+    c, d = -1, -1
+    valido = False
+
+    print(f"Turno: {estado.get_turno().get_turno_actual()}, Moneda: {estado.get_moneda().get_valor_actual()}")
+    if estado.get_turno().get_turno_actual() == 'R' and estado.get_moneda().get_valor_actual() == 'a':
+        print(f"N fichas capturadas: {estado.get_n_fichas().get_ndrc()}")
+        if (estado.get_n_fichas().get_ndrc() >=1 ):
+            # CALCULAR C
+            c = 1
+        
+            # CALCULAR D
+            if n>=0 and n<=5:
+                d = 13-n
+            else:
+                print('No se cumplen las condiciones para calcular d')
+                d = -1
+        else:
+            print('No se cumplen las condiciones para calcular c y d')
+            c = -1
+            d = -1
+    else:
+        print('No es el turno de la ficha')
+        c = -1
+        d = -1
+    
+    print(c, d)
+    if c >= 0 and c <= 1 and d >= 0 and d <= 11:
+        if (estado.get_tablero().estado_casilla(c,d) == 'v') or (estado.get_tablero().estado_casilla(c,d) == 'dro'):
+            valido = True
+        else:
+            valido = False
+    else:
+        valido = False
+    
+    return c, d, valido
+
+# Movimiento de la ficha
+def mover_LDRC(ficha, estado, posiciones):
+    c, d, valido = verificador_LDRC(ficha, estado, posiciones)
+    estadoN = estado
+    if valido:
+        posX, posY = c, d
+        a = ficha.obtenerPosicion()[0]
+        b = ficha.obtenerPosicion()[1]
+
+        # Transformar la coordenada de pantalla en valor del arreglo tablero
+        coord = (a, b)  
+        pos = encontrar_posicion_cercana(coord, posiciones)
+        if pos is not None:
+            print(f'La coordenada {coord} está cerca de la posición {pos} en self.posiciones_fichas')
+            a = pos[0]
+            b = pos[1]
+        else:
+            print(f'La coordenada {coord} no está cerca de ninguna posición en self.posiciones_fichas')
+
+        estadoN = actualizar_estado_LDRC(estado, a, b, posX, posY)
+        print(posX, posY, estadoN.get_tablero().estado_casilla(posX, posY))
+        estadoN.get_tablero().mostrar_tablero()
+        return posX, posY, estadoN
+    else:
+        print('Movimiento no válido')
+        Notifier.notify('Movimiento no válido', title='Error', sound='default')
+        return -1, -1, estadoN
+
+# Actualizar estado del juego
+def actualizar_estado_LDRC(estado, xi, yi, xf, yf):
+    estado.get_turno().cambio_de_turno()
+    estado.get_moneda().esperar_lanzamiento()
+    estado.get_dado().esperar_lanzamiento()
+
+    print("Antigua: ", xi, yi, "Nueva: ", xf, yf)
+    estado.get_n_fichas().eliminar_ficha_drc()
+    
+    # Casilla de destino
+    estado.get_FR().adicionar_ficha_FR(xf, yf)
+    estado.get_tablero().convertir_en_ordinaria(xf, yf, 'R')
+    estado.get_n_fichas().eliminar_ficha_drc()
+    estado.get_n_fichas().adicionar_ficha_dro()
+    estado.get_tablero().mostrar_tablero()
+    
+    estado.mostrar_estado()
+    return estado
+
+# ==================
+#   REGLA 10: LDAC
+# ==================
 
 
 
 
+
+# ==================
+#   REGLA 11: RDRF
+# ==================
+
+
+
+
+# ==================
+#   REGLA 12: RDAF
+# ==================
+
+
+
+
+# ==================
+#   REGLA 13: SDRF
+# ==================
+
+
+
+
+
+# ==================
+#   REGLA 14: SDAF
+# ==================
